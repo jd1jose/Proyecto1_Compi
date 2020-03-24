7ab87cd70d4beab22f63d1c77751c81c4ffc9891
jd1jose <30329994+jd1jose@users.noreply.github.com>
2020-03-24 00:16:27 -0600

    Add files via upload

--- Proyecto1_compi1/Proyecto1_compi1/Generador_AFN_AFD.cs
+++ Proyecto1_compi1/Proyecto1_compi1/Generador_AFN_AFD.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Proyecto1_compi1
 {
@@ -10,6 +11,7 @@ namespace Proyecto1_compi1
     {
 
         public List<string> Expresiones = new List<string>();
+        public List<string> Expresionesnormal = new List<string>();
         public List<string> conjuntos = new List<string>();
         public List<string> tabla_simbolos = new List<string>();
         public List<string> d = new List<string>();
@@ -26,6 +28,7 @@ namespace Proyecto1_compi1
             for (int i = 0; i < conjuntos.Count(); i++) {
                 Console.WriteLine(conjuntos[i]);
             }
+            Pol();
         }
         public void exp() {
             string er = "";
@@ -61,7 +64,7 @@ namespace Proyecto1_compi1
 
                         }
                         else {
-                            er += "@" + lista[0];
+                            er += "@" + lista[0]+"@"+lista[1];
                         }
                     }
                     Expresiones.Add(er);
@@ -134,6 +137,143 @@ namespace Proyecto1_compi1
                 }
                 conjuntos.Add(expandido);
             }
+        }
+
+        private void Pol() {
+            int contador = 1;
+            string etotal = "";
+            string e = "";
+            for (int i=0; i<Expresiones.Count;i++) {
+                
+                int max = maximo(i);
+                int conta = 0;
+             
+                while (conta<max) {
+                    string[] exprecion ;
+                    //un if de cambio de expresion cuando algo haga mach
+                    if (etotal == "" &&conta==0)
+                    {
+                        exprecion = Expresiones[i].Split('@');
+                    }
+                    else {
+                        exprecion = etotal.Split('@');
+                    }
+                    MessageBox.Show("el conta es:"+conta+" y e tiene:"+e+" y estamos ubicados en la lista con:"+exprecion[contador]);
+                    if (exprecion[contador + 1] == "tk_concatenacion" || exprecion[contador + 1] == "tk_or")
+                    {
+                                              
+                        if (exprecion[contador + 3] == "tk_concatenacion" || exprecion[contador + 3] == "tk_or" ||
+                            exprecion[contador + 3] == "tk_masveces" || exprecion[contador + 3] == "tk_ceromasveces" ||
+                            exprecion[contador + 3] == "tk_interrogacion")
+                        {
+                            
+                            contador=contador+2;
+                        }
+                        else
+                        {
+                        
+
+                            if (exprecion[contador + 5] == "tk_concatenacion" || exprecion[contador + 5] == "tk_or" ||
+                                exprecion[contador + 5] == "tk_masveces" || exprecion[contador + 5] == "tk_ceromasveces" ||
+                                exprecion[contador + 5] == "tk_interrogacion")
+                            {
+                                
+                                contador = contador + 2;
+                            }
+                            else
+                            {
+                                e = "@" + exprecion[contador + 2] + exprecion[contador]+ exprecion[contador + 4]+"@tk_cadena";
+                                //mach
+                                MessageBox.Show("lo que tiene e:"+e);
+                                etotal = Loquetenia(contador, i);
+                                MessageBox.Show("etotal solo es:"+etotal);
+                                etotal += e;
+                                MessageBox.Show("como queda etotal entonces: "+etotal);
+                                e = "";
+                                contador = 0;
+                                conta++;
+                            }
+                        }
+
+                    }
+                    else if (exprecion[contador + 1] == "tk_masveces" || exprecion[contador + 1] == "tk_ceromasveces" ||
+                    exprecion[contador + 1] == "tk_interrogacion")
+                    {
+                        
+                        if (exprecion[contador + 3] == "tk_concatenacion" || exprecion[contador + 3] == "tk_or" ||
+                            exprecion[contador + 3] == "tk_masveces" || exprecion[contador + 3] == "tk_ceromasveces" ||
+                            exprecion[contador + 3] == "tk_interrogacion")
+                        {
+                                  
+                            contador = contador + 2;
+                        }
+                        else
+                        {
+                            e = "@" + exprecion[contador+2] + exprecion[contador]+ "@tk_cadena";
+                            //mach
+                            MessageBox.Show("lo que tiene e en else if:" + e);
+                            etotal = Loquetenia(contador,i);
+                            MessageBox.Show("etotal solo es:" + etotal);
+                            etotal += e;
+                            MessageBox.Show("como queda etotal  en el else if entonces: " + etotal);
+                            e = "";
+                            contador = 0;
+                            conta++;
+                        }
+
+
+                    }
+                    else {
+                        contador = contador + 2;
+                    }
+                }
+
+                Console.WriteLine("la expression es: "+etotal);
+                etotal = "";
+                contador = 1;
+            }
+          
+
+        }
+
+        private string Loquetenia(int contador,int i)
+        {
+            string e="";
+            string[] datos = Expresiones[i].Split('@');
+            e = datos[1];
+            for (int j = 2; j <= contador; j++) {
+                if (j == contador) {
+                    if (datos[j] == "tk_concatenacion" || datos[j] == "tk_or" ||
+                            datos[j] == "tk_masveces" || datos[j] == "tk_ceromasveces" ||
+                           datos[j] == "tk_interrogacion" || datos[j]== "tk_cadena")
+                    {
+                        e += "@" + datos[j];
+                    }
+                    else { break; }
+
+                } else {
+                    e += "@" + datos[j];
+                }
+                
+            }
+
+            return e;
+        }
+
+        private int maximo(int i) {
+            int m = 0;
+            int contador = i;
+            string[] d = Expresiones[i].Split('@');
+            for (int j=0; j<d.Length;j++) {
+                
+                if (d[j] == "tk_concatenacion" || d[j] == "tk_or" ||
+                            d[j] == "tk_masveces" || d[j] == "tk_ceromasveces" ||
+                            d[j] == "tk_interrogacion")
+                {
+                    m++;
+                }
+            }
+            return m;
         }
     }
 }
